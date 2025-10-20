4ba72d8077f678310d280345a47084b12ef00a9e
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-20 15:12:09 +0530

    First Commit

--- src/Product.java
+++ src/Product.java
@@ -2,16 +2,16 @@ public class Product {
     private final int id;
     private final String name;
     private int quantity;
-    private final int reorderThreshold;
+    //private final int reorderThreshold;
 
-    public Product(int id, String name, int initialQuantity, int reorderThreshold) {
+    public Product(int id, String name, int initialQuantity, /*int reorderThreshold) */{
         this.id = id;
         this.name = name;
         this.quantity = initialQuantity;
-        this.reorderThreshold = reorderThreshold;
+       // this.reorderThreshold = reorderThreshold;
     }
 
-    public int getId() {
+   /* public int getId() {
          return id;
           }
     public String getName() { 
@@ -37,5 +37,5 @@ public class Product {
 
     public String toString() {
         return String.format("Product[id=%d, name=%s, qty=%d, threshold=%d]", id, name, quantity, reorderThreshold);
-    }
+    }*/
 }
