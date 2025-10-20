e88a9620076fb4acac9fe12dd7ff68267f7c7439
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-20 13:13:15 +0530

    First commit

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Product(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
