5120e0ef519cb9ffb13dbbb8eca9dd6dc97ee100
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-21 21:57:45 +0530

    commit 7

--- src/Product.java
+++ src/Product.java
@@ -2,13 +2,13 @@ public class Product {
     private final int id;
     private final String name;
     private int quantity;
-    //private final int reorderThreshold;
+    private final int reorderThreshold;
 
-    public Product(int id, String name, int initialQuantity, /*int reorderThreshold) */{
+    public Product(int id, String name, int initialQuantity, int reorderThreshold) {
         this.id = id;
         this.name = name;
         this.quantity = initialQuantity;
-       // this.reorderThreshold = reorderThreshold;
+       this.reorderThreshold = reorderThreshold;
     }
 
     public int getId() { return id; }
@@ -38,10 +38,7 @@ public class Product {
         if (amount > this.quantity) throw new IllegalArgumentException("Insufficient stock");
         this.quantity -= amount;
     }
-
-    @Override
     public String toString() {
-        return String.format("Product[id=%d, name=%s, qty=%d, threshold=%d]",
-                id, name, quantity, reorderThreshold);
+        return String.format("Product[id=%d, name=%s, qty=%d, threshold=%d]", id, name, quantity, reorderThreshold);
     }
 }
